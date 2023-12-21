d25dd301af67336f2bc32616c4f907b22a917da0
Alko89 <alefer89@gmail.com>
2023-12-21 12:40:22 +0100

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,75 @@
+import AccountCard from '@/components/AccountCard'
+import AccountPicker from '@/components/AccountPicker'
+import PageHeader from '@/components/PageHeader'
+import { EIP155_MAINNET_CHAINS, EIP155_TEST_CHAINS } from '@/data/EIP155Data'
+import SettingsStore from '@/store/SettingsStore'
+import { Text } from '@nextui-org/react'
+import { Fragment } from 'react'
+import { useSnapshot } from 'valtio'
+import SmartAccountCard from '@/components/SmartAccountCard'
+
+export default function HomePage() {
+  const {
+    testNets,
+    eip155Address,
+    activeChainId
+  } = useSnapshot(SettingsStore.state)
+
+  return (
+    <Fragment>
+      <PageHeader title="Accounts">
+        <AccountPicker data-testid="account-picker" />
+      </PageHeader>
+      <Text h4 css={{ marginBottom: '$5' }}>
+        Mainnets
+      </Text>
+      {Object.entries(EIP155_MAINNET_CHAINS).map(([caip10, { name, logo, rgb }]) => (
+        <AccountCard
+          key={name}
+          name={name}
+          logo={logo}
+          rgb={rgb}
+          address={eip155Address}
+          chainId={caip10.toString()}
+          data-testid={'chain-card-' + caip10.toString()}
+        />
+      ))}
+
+      {testNets ? (
+        <Fragment>
+          <Text h4 css={{ marginBottom: '$5' }}>
+            Testnets
+          </Text>
+          {Object.entries(EIP155_TEST_CHAINS).map(([caip10, { name, logo, rgb, chainId, smartAccountEnabled }]) => {
+            if (smartAccountEnabled) {
+              return (
+                <SmartAccountCard
+                  key={name}
+                  name={name}
+                  logo={logo}
+                  rgb={rgb}
+                  address={eip155Address}
+                  chainId={caip10.toString()}
+                  data-testid={'chain-card-' + caip10.toString()}
+                  isActiveChain={activeChainId === `eip155:${chainId}`}
+                />
+              )
+            }
+
+            return (
+              <AccountCard
+                key={name}
+                name={name}
+                logo={logo}
+                rgb={rgb}
+                address={eip155Address}
+                chainId={caip10.toString()}
+                data-testid={'chain-card-' + caip10.toString()}
+              />
+            )
+          })}
+        </Fragment>
+      ) : null}
+    </Fragment>
+  )
+}
---
+++ src/views/SessionProposalModal.tsx
@@ -0,0 +1,224 @@
+import { Col, Divider, Grid, Row, Text, styled } from '@nextui-org/react'
+import { Fragment, useCallback, useMemo } from 'react'
+import { buildApprovedNamespaces, getSdkError } from '@walletconnect/utils'
+
+import DoneIcon from '@mui/icons-material/Done'
+import CloseIcon from '@mui/icons-material/Close'
+
+import ProjectInfoCard from '@/components/ProjectInfoCard'
+import RequestModalContainer from '@/components/RequestModalContainer'
+import ModalStore from '@/store/ModalStore'
+import { eip155Addresses } from '@/utils/EIP155WalletUtil'
+import { styledToast } from '@/utils/HelperUtil'
+import { web3wallet } from '@/utils/WalletConnectUtil'
+import { EIP155_CHAINS, EIP155_SIGNING_METHODS } from '@/data/EIP155Data'
+import ChainDataMini from '@/components/ChainDataMini'
+import ChainAddressMini from '@/components/ChainAddressMini'
+import { getChainData } from '@/data/chainsUtil'
+import VerifyInfobox from '@/components/VerifyInfobox'
+import ModalFooter from '@/components/ModalFooter'
+import RequestModal from './RequestModal'
+
+const StyledText = styled(Text, {
+  fontWeight: 400
+} as any)
+
+const StyledSpan = styled('span', {
+  fontWeight: 400
+} as any)
+
+export default function SessionProposalModal() {
+  // Get proposal data and wallet address from store
+  const proposal = ModalStore.state.data?.proposal
+  console.log('proposal', proposal)
+  const supportedNamespaces = useMemo(() => {
+    // eip155
+    const eip155Chains = Object.keys(EIP155_CHAINS)
+    const eip155Methods = Object.values(EIP155_SIGNING_METHODS)
+
+    return {
+      eip155: {
+        chains: eip155Chains,
+        methods: eip155Methods,
+        events: ['accountsChanged', 'chainChanged'],
+        accounts: eip155Chains.map(chain => `${chain}:${eip155Addresses[0]}`).flat()
+      },
+    }
+  }, [])
+  console.log('supportedNamespaces', supportedNamespaces, eip155Addresses)
+
+  const requestedChains = useMemo(() => {
+    if (!proposal) return []
+    const required = []
+    for (const [key, values] of Object.entries(proposal.params.requiredNamespaces)) {
+      const chains = key.includes(':') ? key : values.chains
+      required.push(chains)
+    }
+
+    const optional = []
+    for (const [key, values] of Object.entries(proposal.params.optionalNamespaces)) {
+      const chains = key.includes(':') ? key : values.chains
+      optional.push(chains)
+    }
+    console.log('requestedChains', [...new Set([...required.flat(), ...optional.flat()])])
+    return [...new Set([...required.flat(), ...optional.flat()])]
+  }, [proposal])
+
+  // the chains that are supported by the wallet from the proposal
+  const supportedChains = useMemo(
+    () => requestedChains.map(chain => getChainData(chain!)),
+    [requestedChains]
+  )
+
+  // get required chains that are not supported by the wallet
+  const notSupportedChains = useMemo(() => {
+    if (!proposal) return []
+    const required = []
+    for (const [key, values] of Object.entries(proposal.params.requiredNamespaces)) {
+      const chains = key.includes(':') ? key : values.chains
+      required.push(chains)
+    }
+    return required
+      .flat()
+      .filter(
+        chain =>
+          !supportedChains
+            .map(supportedChain => `${supportedChain?.namespace}:${supportedChain?.chainId}`)
+            .includes(chain!)
+      )
+  }, [proposal, supportedChains])
+  console.log('notSupportedChains', notSupportedChains)
+  const getAddress = useCallback((namespace?: string) => {
+    if (!namespace) return 'N/A'
+    switch (namespace) {
+      case 'eip155':
+        return eip155Addresses[0]
+    }
+  }, [])
+
+  const approveButtonColor: any = useMemo(() => {
+    switch (proposal?.verifyContext.verified.validation) {
+      case 'INVALID':
+        return 'error'
+      case 'UNKNOWN':
+        return 'warning'
+      default:
+        return 'success'
+    }
+  }, [proposal])
+
+  // Ensure proposal is defined
+  if (!proposal) {
+    return <Text>Missing proposal data</Text>
+  }
+
+  // Get required proposal data
+  const { id, params } = proposal
+
+  const { proposer, relays } = params
+
+  // Hanlde approve action, construct session namespace
+  async function onApprove() {
+    if (proposal) {
+      const namespaces = buildApprovedNamespaces({
+        proposal: proposal.params,
+        supportedNamespaces
+      })
+
+      console.log('approving namespaces:', namespaces)
+
+      try {
+        await web3wallet.approveSession({
+          id,
+          relayProtocol: relays[0].protocol,
+          namespaces
+        })
+      } catch (e) {
+        styledToast((e as Error).message, 'error')
+        return
+      }
+    }
+    ModalStore.close()
+  }
+
+  // Hanlde reject action
+  async function onReject() {
+    if (proposal) {
+      try {
+        await web3wallet.rejectSession({
+          id,
+          reason: getSdkError('USER_REJECTED_METHODS')
+        })
+      } catch (e) {
+        styledToast((e as Error).message, 'error')
+        return
+      }
+    }
+    ModalStore.close()
+  }
+
+  return (
+    <RequestModal
+      metadata={proposal.params.proposer.metadata}
+      onApprove={onApprove}
+      onReject={onReject}
+      infoBoxCondition={notSupportedChains.length > 0}
+      infoBoxText={`The following required chains are not supported by your wallet - ${notSupportedChains.toString()}`}
+      disabledApprove={notSupportedChains.length > 0}
+    >
+      <Row>
+        <Col>
+          <StyledText h4>Requested permissions</StyledText>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <DoneIcon style={{ verticalAlign: 'bottom' }} />{' '}
+          <StyledSpan>View your balance and activity</StyledSpan>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <DoneIcon style={{ verticalAlign: 'bottom' }} />{' '}
+          <StyledSpan>Send approval requests</StyledSpan>
+        </Col>
+      </Row>
+      <Row>
+        <Col style={{ color: 'gray' }}>
+          <CloseIcon style={{ verticalAlign: 'bottom' }} />
+          <StyledSpan>Move funds without permission</StyledSpan>
+        </Col>
+      </Row>
+      <Grid.Container style={{ marginBottom: '10px', marginTop: '10px' }} justify={'space-between'}>
+        <Grid>
+          <Row style={{ color: 'GrayText' }}>Accounts</Row>
+          {supportedChains.length &&
+            supportedChains.map((chain, i) => {
+              return (
+                <Row key={i}>
+                  <ChainAddressMini key={i} address={getAddress(chain?.namespace)} />
+                </Row>
+              )
+            })}
+        </Grid>
+        <Grid>
+          <Row style={{ color: 'GrayText' }} justify="flex-end">
+            Chains
+          </Row>
+          {supportedChains.length &&
+            supportedChains.map((chain, i) => {
+              if (!chain) {
+                return <></>
+              }
+
+              return (
+                <Row key={i}>
+                  <ChainDataMini key={i} chainId={`${chain?.namespace}:${chain?.chainId}`} />
+                </Row>
+              )
+            })}
+        </Grid>
+      </Grid.Container>
+    </RequestModal>
+  )
+}
